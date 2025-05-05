d93184d98e3bfd54808a3308268d2ea9110172aa
hsrokon <hsrokonofficial@gmail.com>
2025-05-05 11:35:02 +0600

    footer created and designed from scratch

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-            <div>
-                <h1>joijioj</h1>
+        <footer className='w-11/12 mx-auto mt-10 text-center text-sm py-16'>
+            <div className='my-6'>
+                <h2 className='text-2xl font-bold my-2'>Gadgetorium</h2>
+                <p>Explore & Find Your Perfect Tech Match</p>
             </div>
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
+            <hr className='text-gray-300 w-8/12 mx-auto'/>
+            <div className='flex gap-20 justify-center my-6 text-gray-500'>
+                <div className=' flex flex-col gap-2'>
+                    <Link><h3 className='text-black font-semibold'>Services</h3></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Product Support</p></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Order Tracking</p></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Shipping & Delivery</p></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Returns</p></Link>
+                </div>
+                <div className=' flex flex-col gap-2'>
+                    <Link><h3 className='text-black font-semibold'>Company</h3></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>About Us</p></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Careers</p></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Contact</p></Link>
+                </div>
+                <div className=' flex flex-col gap-2'>
+                    <Link><h3 className='text-black font-semibold'>Legal</h3></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Terms of Service</p></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Privacy Policy</p></Link>
+                    <Link><p className='hover:text-gray-800 hover:underline transition'>Cookie Policy</p></Link>
+                </div>
+            </div>
         </footer>
     );
 };
